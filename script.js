87ebdee14de90289865d2697c22f01123c7cd4e2
Alexandra Colorado <alexandrapena19@gmail.com>
2023-09-03 00:05:38 -0400

    Added Event onKeyUp

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-const input = document.querySelector('#fruit');
+let inputBox = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+
+inputBox.onkeyup = function(){
+	let results = [];
+	let input = inputBox.value.toLowerCase();
+	if(input.length){
+		results = fruit.filter((keyword) => keyword.toLowerCase().includes(input));
+		console.log();
+	}
+	showSuggestions(results)
+}
+
+function searchHandler(e) {
+	// TODO
+}
+
+function showSuggestions(results) {
+	const content = results.map((list)=>{ 
+		return "<li onclick=useSuggestion(this)>" + list + "</li>";
+		console.log(content)
+	});
+
+	suggestions.innerHTML = "<ul>" + content + "</ul>";
+}
+
+function useSuggestion(list) {
+	inputBox = list.innerHTML;
+	
+}
+
+// input.addEventListener('keyup', searchHandler);
+// suggestions.addEventListener('click', useSuggestion);
 
 function search(str) {
 	let results = [];
