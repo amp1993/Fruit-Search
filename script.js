2c50edc60634dc20c90ac8ee2ed7c97a63f936b9
Alexandra Colorado <alexandrapena19@gmail.com>
2023-09-03 22:37:05 -0400

    Function and Events added

--- script.js
+++ script.js
@@ -1,60 +1,43 @@
-let inputBox = document.querySelector('#fruit');
+const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
+
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 
-inputBox.onkeyup = function(){
-	let results = [];
-	let input = inputBox.value.toLowerCase();
-	if(input.length){
-		results = fruit.filter((keyword) => keyword.toLowerCase().includes(input));
-		console.log();
-	}
-	showSuggestions(results)
-}
+input.addEventListener('keyup', searchHandler);
 
-function searchHandler(e) {
-	// TODO
-}
-
-function showSuggestions(results) {
-	const content = results.map((list)=>{ 
-		return "<li onclick=useSuggestion(this)>" + list + "</li>";
-		console.log(content)
-	});
-
-	suggestions.innerHTML = "<ul>" + content + "</ul>";
-}
-
-function useSuggestion(list) {
-	inputBox = list.innerHTML;
-	
-}
-
-// input.addEventListener('keyup', searchHandler);
-// suggestions.addEventListener('click', useSuggestion);
 
 function search(str) {
 	let results = [];
-
-	// TODO
-
+	results = fruit.filter((keyword) => keyword.toLowerCase().includes(str));
 	return results;
 }
 
 function searchHandler(e) {
-	// TODO
+	const inputEntered = input.value;
+	const results = search(inputEntered);
+	showSuggestions(results, inputEntered);
+	
 }
 
 function showSuggestions(results, inputVal) {
+	suggestions.innerHTML = '';
 
-	// TODO
+	results.forEach((results) => {
+	const liElement = document.createElement('li');
+	const ulElement = document.querySelector('ul');
+	ulElement.appendChild(liElement);
+	liElement.textContent = results;
+
+	liElement.addEventListener('click', function(){
+		input.value = results;
+		suggestions.innerHTML = '';
+
+	});
+});
 }
+  
 
-function useSuggestion(e) {
-	// TODO
-}
 
-input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
